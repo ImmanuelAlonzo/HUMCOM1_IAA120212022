8d3e77abdaefa0fbb11c6f3600d703a9dbe7220b
Immanuel Alonzo <98298870+ImmanuelAlonzo@users.noreply.github.com>
2022-05-13 19:43:49 +0800

    Add files via upload

---
+++ Exercise15-17/Exercise15/Script15.js
@@ -0,0 +1,6 @@
+document.write("Immanuel S. Alonzo <br>");
+document.write("BS Computer Science <br>");
+document.write("1st year <br>");
+document.write("HUMCOM1 <br>");
+document.write("IAA1 <br>");
+document.write("Human-computer interaction <br>");
