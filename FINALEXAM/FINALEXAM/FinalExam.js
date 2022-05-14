c478094b9304de41025f75aae3a6e2db8e093315
Immanuel Alonzo <98298870+ImmanuelAlonzo@users.noreply.github.com>
2022-05-14 21:37:58 +0800

    Add files via upload

---
+++ FINALEXAM/FINALEXAM/FinalExam.js
@@ -0,0 +1,20 @@
+const myName="Alonzo, Immanuel S."
+const myEmail="20198087@s.ubaguio.edu"
+const myProgram="Computer Science"
+const myID="20198087"
+const myYear="1st Year"
+const mySection="IAA1"
+
+console.log('Name: ' + myName);
+console.log('Email Address: ' + myEmail);
+console.log('Program: ' + myProgram);
+console.log('ID Number: ' + myID);
+console.log('Year Level: ' + myYear);
+console.log('Section: ' + mySection);
+
+var myWebsite="Blog"
+var author="Alonzo, Immanuel S."
+
+console.log('Final Project/Exam');
+console.log('Website Name: ' + myWebsite);
+console.log('Author: ' + author);
