3339e9756f783468c6db7ece9bda3c624480844a
Immanuel Alonzo <98298870+ImmanuelAlonzo@users.noreply.github.com>
2022-05-15 14:41:40 +0800

    Update Script15.js

--- Exercise15-17/Exercise15/Script15.js
+++ Exercise15-17/Exercise15/Script15.js
@@ -1,6 +1,18 @@
-document.write("Immanuel S. Alonzo <br>");
-document.write("BS Computer Science <br>");
-document.write("1st year <br>");
-document.write("HUMCOM1 <br>");
-document.write("IAA1 <br>");
-document.write("Human-computer interaction <br>");
+var fname="Immanuel"
+var age="19"
+var phone="09XXXXXXXXX"
+var email="20198087@s.ubaguio.edu"
+var mail="Agoo, La Union"
+var program="Computer Science"
+var ID="20198087"
+var year="1st year"
+var section="IAA1"
+console.log('First Name: '+ fname);
+console.log('Age: '+ age);
+console.log('Phone Number: '+ phone);
+console.log('Email Address: '+ email);
+console.log('Mailing Address: '+ mail);
+console.log('Program: '+ program);
+console.log('ID Number: '+ ID);
+console.log('Year Level: '+ year);
+console.log('Section: '+ section);
